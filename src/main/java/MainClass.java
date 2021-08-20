b004f4aaa1d18814e540b1124c1131121da8cde2
osama <osama.salman99@gmail.com>
2021-08-20 20:05:28 +0300

    Minor refactoring

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,9 +1,9 @@
 public class MainClass {
-	public static void main(String[] args) {
-		for (String arg : args) {
-			if (arg.replace(" ", "").equals("-version")) {
-				System.out.println("PDF Tesseract Beta (1.0)");
-			}
-		}
-	}
+    public static void main(String[] args) {
+        for (String arg : args) {
+            if (arg.replace(" ", "").equals("-version")) {
+                System.out.println("PDF Tesseract Beta (1.0)");
+            }
+        }
+    }
 }
